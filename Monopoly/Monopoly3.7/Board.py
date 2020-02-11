1131ef626d21e7db37dc62496753e51f5a2bbdc0
aulee888 <aulee5451@gmail.com>
2020-02-10 23:05:32 -0800

    Added names of properties

--- Monopoly/Monopoly3.7/Board.py
+++ Monopoly/Monopoly3.7/Board.py
@@ -4,64 +4,79 @@ class Property:
         self.hotel = 0
 
     def __str__(self):
-        return (f'Property: {self.name} \n' \
-               f'Neighborbood: {self.color} \n' \
+        return (f'Property: {self._name} \n' \
+               f'Neighborbood: {self._color} \n' \
                f'Houses: {self.houses} \n' \
                f'Hotel: {self.hotel}')
+
 
 class Brown(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color = 'Brown'
+        self._name = name
+        self._color = 'Brown'
+
 
 class LiteBlue(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color =  'LiteBlue'
+        self._name = name
+        self._color = 'LiteBlue'
+
 
 class Purple(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color = 'Purple'
+        self._name = name
+        self._color = 'Purple'
+
 
 class Orange(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color = 'Orange'
+        self._name = name
+        self._color = 'Orange'
+
 
 class Red(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color = 'Red'
+        self._name = name
+        self._color = 'Red'
+
 
 class Yellow(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color = 'Yellow'
+        self._name = name
+        self._color = 'Yellow'
+
 
 class Green(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color = 'Green'
+        self._name = name
+        self._color = 'Green'
+
 
 class Blue(Property):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.color = 'Blue'
+        self._name = name
+        self._color = 'Blue'
+
+
+def prop_creator():
+    pass
+
 
 board = {}
-lite_blue_property = ['Mediterranean Ave.', 'Baltic Ave.', 'Oriental Ave.']
-for property in lite_blue_property:
-    board[property].append(LiteBlue(property))
+brown_prop = ['Mediterranean Ave.', 'Baltic Ave.']
+lite_blue_prop = ['Oriental Ave.', 'Vermont Ave.', 'Connecticut Ave.']
+purple_prop = ['St. Charles Place', 'States Ave.', 'Virginia Ave.']
+orange_prop = ['St. James Place', 'Tennessee Ave.', 'New York Ave.']
+red_prop = ['Kentucky Ave.', 'Indiana Ave.', 'Illinois Ave.']
+yellow_prop = ['Atlantic Ave.', 'Ventnor Ave.', 'Marvin Gardens']
+green_prop = ['Pacific Ave', 'North Carolina Ave.', 'Pennsylvania Ave.']
+blue_prop = ['Park Place', 'Boardwalk']
 
-for property in board:
-    print(board[property])
-    print('')
