8c2305e6a86fbfa17166744de55699474eebeb54
aulee888 <aulee5451@gmail.com>
2020-02-15 17:02:05 -0500

    Completed placeholder board, streets/utilities/railroads have get_rent

--- Monopoly/Monopoly3.7/Board.py
+++ Monopoly/Monopoly3.7/Board.py
@@ -1,82 +1,168 @@
+import pandas as pd
+# from community_chest import community_chest
+# from chance import chance
+
+
 class Property:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.owner = None
+        self.mortgaged = False
+
+    def __str__(self):
+        return (f'Property: {self.name} \n'
+                f'Owned: {self.owner} \n'
+                f'Mortgaged: {self.mortgaged}')
+
+
+class Street(Property):
+    def __init__(self, name, color, cost, base_rent, upgrade_cost,
+                 rent1, rent2, rent3, rent4, rent5):
+        super().__init__(name)
+        self.color = color
+        self.cost = cost
+        self.base_rent = base_rent
+        self.upgrade_cost = upgrade_cost
+        self.rent1 = rent1
+        self.rent2 = rent2
+        self.rent3 = rent3
+        self.rent4 = rent4
+        self.rent5 = rent5
         self.houses = 0
         self.hotel = 0
 
+    def get_rent(self):
+        if self.hotel == 1:
+            return self.rent5
+
+        if self.houses == 0:
+            return self.base_rent
+        elif self.houses == 1:
+            return self.rent1
+        elif self.houses == 2:
+            return self.rent2
+        elif self.houses == 3:
+            return self.rent3
+        elif self.houses == 4:
+            return self.rent4
+
     def __str__(self):
-        return (f'Property: {self._name} \n' \
-               f'Neighborbood: {self._color} \n' \
-               f'Houses: {self.houses} \n' \
-               f'Hotel: {self.hotel}')
+        return (f'Property: {self.name} \n' \
+                f'Color: {self.color} \n' \
+                f'Houses: {self.houses} \n' \
+                f'Hotel: {self.hotel} \n' \
+                f'Owned: {self.owner} \n' \
+                f'Mortgaged: {self.mortgaged}')
 
 
-class Brown(Property):
+class RailRoad(Property):
+    rr_owners = []
+
     def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'Brown'
+        super().__init__(name)
+        self.cost = 200
+
+    def get_rent(self):
+        self.count = 0
+
+        for owner in self.rr_owners:
+            if owner == self.owner:
+                self.count += 1
+
+        return 25 * 2**(self.count - 1)  # Formula for 25, 50, 100, 200
 
 
-class LiteBlue(Property):
+class Utility(Property):
+    utility_owners = []
+
     def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'LiteBlue'
+        super().__init__(name)
+        self.cost = 150
+
+    def get_rent(self):
+        self.count = 0
+
+        for owner in self.utility_owners:
+            if owner == self.owner:
+                self.count += 1
+
+        if self.count == 1:
+            return 4 * last_move
+
+        elif self.count == 2:
+            return 10 * last_move
 
 
-class Purple(Property):
-    def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'Purple'
+class Chance:
+    def __init__(self):
+        self.name = 'Chance'
 
 
-class Orange(Property):
-    def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'Orange'
+class CommunityChest:
+    def __init__(self):
+        self.name = 'Community Chest'
 
 
-class Red(Property):
-    def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'Red'
+class IncomeTax:
+    def __init__(self):
+        self.name = 'Income Tax'
 
 
-class Yellow(Property):
-    def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'Yellow'
+class LuxuryTax:
+    def __init__(self):
+        self.name = 'Luxury Tax'
 
 
-class Green(Property):
-    def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'Green'
+class Jail:
+    def __init__(self):
+        self.name = 'Jail'
 
 
-class Blue(Property):
-    def __init__(self, name):
-        super().__init__()
-        self._name = name
-        self._color = 'Blue'
+class Go:
+    def __init__(self):
+        self.name = 'Go'
 
 
-def prop_creator():
-    pass
+class FreeParking:
+    def __init__(self):
+        self.name = FreeParking
+        self.money = 0
 
 
-board = {}
-brown_prop = ['Mediterranean Ave.', 'Baltic Ave.']
-lite_blue_prop = ['Oriental Ave.', 'Vermont Ave.', 'Connecticut Ave.']
-purple_prop = ['St. Charles Place', 'States Ave.', 'Virginia Ave.']
-orange_prop = ['St. James Place', 'Tennessee Ave.', 'New York Ave.']
-red_prop = ['Kentucky Ave.', 'Indiana Ave.', 'Illinois Ave.']
-yellow_prop = ['Atlantic Ave.', 'Ventnor Ave.', 'Marvin Gardens']
-green_prop = ['Pacific Ave', 'North Carolina Ave.', 'Pennsylvania Ave.']
-blue_prop = ['Park Place', 'Boardwalk']
+def setup():
+    board = {}
 
+    properties_df = pd.read_excel('Properties_Data.xlsx')
+    for i in range(len(properties_df)):
+        curr = properties_df.loc[i]
+        board[curr['position']] = Street(curr['name'], curr['color'],
+                                           curr['cost'], curr['base_rent'],
+                                           curr['upgrade_cost'],
+                                           curr['rent1'],curr['rent2'],
+                                           curr['rent3'], curr['rent4'],
+                                           curr['rent5'])
+
+    board[5] = RailRoad('Reading Railroad')
+    board[15] = RailRoad('Pennsylvania Railroad')
+    board[25] = RailRoad('B. & O. Railroad')
+    board[35] = RailRoad('Short Line Railroad')
+
+    board[12] = Utility('Electric Company')
+    board[28] = Utility('Water Works')
+
+    board[7] = Chance()
+    board[22] = Chance()
+    board[36] = Chance()
+
+    board[2] = CommunityChest()
+    board[17] = CommunityChest()
+    board[33] = CommunityChest()
+
+    board[4] = IncomeTax()
+    board[38] = LuxuryTax()
+
+    board[0] = Go()
+    board[10] = Jail()
+    board[20] = FreeParking()
+
+    return board
