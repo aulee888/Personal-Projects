b20077066528402a8e319cd640e163bdd5225a14
aulee888 <aulee5451@gmail.com>
2020-03-05 07:31:03 -0800

    Completed box_check and successful testing

--- Sudoku/Validation.py
+++ Sudoku/Validation.py
@@ -1,38 +1,55 @@
 class Validate:
-    def __init__(self, board):
-        self.board = board
-
-    def col_check(self):
+    def col_check(self, board):
         for j in range(9):
             used = set()
 
             for i in range(9):
-                if self.board[i][j] in used:
+                if board[i][j] in used:
                     return False
                 else:
-                    used.add(self.board[i][j])
+                    used.add(board[i][j])
 
         return True
 
-    def row_check(self):
+    def row_check(self, board):
         for i in range(9):
             used = set()
 
             for j in range(9):
-                if self.board[i][j] in used:
+                if board[i][j] in used:
                     return False
                 else:
-                    used.add(self.board[i][j])
+                    used.add(board[i][j])
 
         return True
 
-    def box_check(self):
-        for k in range(3):
-            used = set()
+    def box_check(self, board):
+        for p in range(3):
+            for q in range(3):
+                used = set()
 
-            for i in range(3):
-                for j in range(3):
-                    if self.board[3*k + i][2*k + j] in used:
-                        return False
+                for i in range(3):
+                    for j in range(3):
+                        value = board[3*p + i][3*q + j]
+
+                        if value in used:
+                            return False
+                        else:
+                            used.add(value)
+
+        return True
 
 
+test = [[8, 2, 7, 1, 5, 4, 3, 9, 6],
+        [9, 6, 5, 3, 2, 7, 1, 4, 8],
+        [3, 4, 1, 6, 8, 9, 7, 5, 2],
+        [5, 9, 3, 4, 6, 8, 2, 7, 1],
+        [4, 7, 2, 5, 1, 3, 6, 8, 9],
+        [6, 1, 8, 9, 7, 2, 4, 3, 5],
+        [7, 8, 6, 2, 3, 5, 9, 1, 4],
+        [1, 5, 4, 7, 9, 6, 8, 2, 3],
+        [2, 3, 9, 8, 4, 1, 5, 6, 7]]
+
+print(Validate().col_check(test))
+print(Validate().row_check(test))
+print(Validate().box_check(test))
